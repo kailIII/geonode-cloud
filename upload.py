bb08a556a9c5cc61b8ccc609aabe7e7d1c0d74dc
Jeffrey Johnson <jjohnson@opengeo.org>
2011-03-17 10:29:57 -0700

    Set public in upload.py

--- upload.py
+++ upload.py
@@ -11,5 +11,6 @@ bucket = conn.get_bucket(bucket_name)
 k = Key(bucket)
 k.key = file_name.split('/')[-1]
 k.set_contents_from_filename(file_name)
+k.set_acl('public-read')
 
 print file_name + " uploaded to " + bucket_name
